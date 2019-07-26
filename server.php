49a0b353ec835873e16529096d76d7a677b13a27
deandre428 <51965021+deandre428@users.noreply.github.com>
2019-07-25 22:28:20 -0400

    Update server.php

--- server.php
+++ server.php
@@ -1,10 +1,36 @@
 <?php
 session_start();
 
+
 // initializing variables
 $username = "";
 $email    = "";
 $errors = array();
+
+class DBController {
+	 private $host = "den1.mysql6.gear.host ";
+	 private $user = "travelagent";
+    private $password = "travelagent!";
+	 private $database = "travelagent";
+	       private $conn;
+	
+        function __construct() {
+        $this->conn = $this->connectDB();
+	}	
+	function connectDB() {
+		$conn = mysqli_connect($this->host,$this->user,$this->password,$this->database);
+		return $conn;
+	}
+        function runQuery($query) {
+                $result = mysqli_query($this->conn,$query);
+                while($row=mysqli_fetch_assoc($result)) {
+                $resultset[] = $row;
+                }		
+                if(!empty($resultset))
+                return $resultset;
+	}
+
+
 
 
 
